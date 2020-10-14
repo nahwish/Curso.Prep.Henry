62103d0dce2405d8094b4fbf30914e618ea81a69
chawi reds <nahudaro@gmails.com>
2020-10-14 20:48:25 -0300

    here! try try and try again, adds tree files

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,17 +3,19 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
-  
+  return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.lenght;
 }
 
 function incrementarPorUno(array) {
@@ -21,12 +23,14 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  return array[]
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push[elemento];
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -74,6 +78,13 @@ function multiplicarArgumentos() {
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
 }
+
+for( i = 0; i <= 10; i++ ){
+  
+}
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
